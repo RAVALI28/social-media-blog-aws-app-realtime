f2db0cf5acccd03e08cb082c26402b8b11c5f72e
RAVALI28 <ravalikavajinapally.rv@gmail.com>
2025-03-12 18:47:55 -0500

    Added sorting implemenation to the post resource

--- src/main/java/com/spring/learning/social_media_blog_app/Controller/PostController.java
+++ src/main/java/com/spring/learning/social_media_blog_app/Controller/PostController.java
@@ -29,9 +29,11 @@ public class PostController {
     @GetMapping("/getAll")
     public PostResponse getAllPosts(
             @RequestParam(value = "pageNo", defaultValue = "0", required = false) int  pageNo,
-            @RequestParam(value = "pageSize", defaultValue = "10", required = false) int pageSize
+            @RequestParam(value = "pageSize", defaultValue = "10", required = false) int pageSize,
+            @RequestParam(value = "sortBy", defaultValue = "id", required = false) String sortBy,
+            @RequestParam(value = "sortDir", defaultValue = "id", required = false) String sortDir
     ){
-       return postService.getAllPosts(pageNo, pageSize);
+       return postService.getAllPosts(pageNo, pageSize, sortBy, sortDir);
     }
 
 
